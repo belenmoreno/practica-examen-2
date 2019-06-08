6d93bd41e2c43077509ffe2343512f4f311f06fd
Belén Moreno Martín <b.moreno22@usp.ceu.es>
2019-06-08 18:41:52 +0200

    ejercicio1

--- ej1.py
+++ ej1.py
@@ -4,12 +4,14 @@ def main():
     aux = 0
     numero_estrellas = 1
     i = 0
+
     while (aux < numero):#numero de filas que pintamos
             while (i < numero_estrellas):#numero estrellas que pintamos
                 print("*", end = '')
-            print("\n")
-            i = 0
+                i += 1
+            print("")
             numero_estrellas += 2
+            i = 0
             aux += 1
         
     
