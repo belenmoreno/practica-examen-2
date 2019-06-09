4d0e2f952b1c97b1947d94257c50da8d2d039888
Sergio <marinobeach@gmail.com>
2019-06-09 13:12:23 +0200

    feat: exercise 1 finished

--- ej1.py
+++ ej1.py
@@ -1,20 +1,27 @@
 def main():
 
-    numero = int(input("introduce un numero entero: "))
-    aux = 0
+    numero_filas = int(input("introduce un numero entero: "))
+    contador_filas_actuales = 0
     numero_estrellas = 1
     i = 0
+    j = 0
 
-    while (aux < numero):#numero de filas que pintamos
-            while (i < numero_estrellas):#numero estrellas que pintamos
-                print("*", end = '')
-                i += 1
-            print("")
-            numero_estrellas += 2
-            i = 0
-            aux += 1
-        
-    
+    while contador_filas_actuales < numero_filas:#numero de filas que pintamos
+
+        espacios = numero_filas - (contador_filas_actuales + 1)
+        while j < espacios:#numero espacios que pintamos
+            print(" ", end='')
+            j += 1
+
+        while i < numero_estrellas:#numero estrellas que pintamos
+            print("*", end='')
+            i += 1
+
+        print("")
+        numero_estrellas += 2
+        i = 0
+        j = 0
+        contador_filas_actuales += 1
 
 
 if __name__ == "__main__":
